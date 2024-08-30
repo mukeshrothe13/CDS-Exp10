9e16a462426a89adbccae2c8d0f0258855bae6a7
mukeshrothe13 <rothemukesh7@gmail.com>
2024-08-30 23:07:31 +0530

    10A_callbyvalue.cpp

---
+++ 10A_callbyvalue.cpp
@@ -0,0 +1,17 @@
+//Mukesh Rothe 23070123089 EXP10
+#include <iostream>
+using namespace std;
+void swap(int *a,int* b)
+{
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+  cout<<"Inside swabByValue function: "<<"a = "<< *a<<" , "<<"b = "<< *b<<endl;
+}
+ int main()
+{ int a=5,b=10;
+cout<<"Before swabByValue- "<<"a = "<<a<<" , "<<"b = "<<b<<endl;
+swap(&a,&b); 
+cout<<"After swapByValue- "<<"a = "<<a<<" , "<<"b = "<<b<<endl;
+}
